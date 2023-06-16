9d4897efbcbe143e89a59307f0566ced1767a520
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-06-16 13:06:00 -0700

    memo done

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
@@ -12,9 +14,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://sakurawtj:<password>@memoryproject.8giccfm.mongodb.net/?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://<username>:<password>@memoryproject.8giccfm.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5001;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));

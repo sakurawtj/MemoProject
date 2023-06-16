0b25f6fc1a2f0482b3e1acda541dab1da7d6f204
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-06-16 12:16:32 -0700

    project done

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://sakurawtj:sakurawtj123@memoryproject.8giccfm.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://sakurawtj:<password>@memoryproject.8giccfm.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5001;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
